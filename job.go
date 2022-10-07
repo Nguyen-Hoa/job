d45ce026b823a2c91090f0108e148e98ca67deff
kimchi <hnguyen257@ucmerced.edu>
2022-10-07 12:34:36 -0700

    sharedJobsArray

--- job.go
+++ job.go
@@ -1,7 +1,6 @@
-package worker
+package job
 
 import (
-	"sync"
 	"time"
 
 	"github.com/docker/docker/api/types"
@@ -27,56 +26,4 @@ type DockerJob struct {
 func (j *BaseJob) UpdateTotalRunTime(time.Time) error {
 	j.TotalRunTime += time.Since(j.StartTime)
 	return nil
-}
-
-type SharedJobsArray struct {
-	mu   sync.Mutex
-	jobs []Job
-}
-
-func (j *SharedJobsArray) Get(index int) Job {
-	var job Job
-	j.mu.Lock()
-	job = j.jobs[index]
-	j.mu.Unlock()
-	return job
-}
-
-func (j *SharedJobsArray) Pop() Job {
-	var job Job
-	j.mu.Lock()
-	job, j.jobs = j.jobs[0], j.jobs[1:]
-	j.mu.Unlock()
-	return job
-}
-
-func (j *SharedJobsArray) Append(job Job) {
-	j.mu.Lock()
-	j.jobs = append(j.jobs, job)
-	j.mu.Unlock()
-}
-
-type SharedDockerJobsMap struct {
-	mu   sync.Mutex
-	jobs map[string]DockerJob
-}
-
-func (j *SharedDockerJobsMap) Get(key string) DockerJob {
-	var job DockerJob
-	j.mu.Lock()
-	job = j.jobs[key]
-	j.mu.Unlock()
-	return job
-}
-
-func (j *SharedDockerJobsMap) Delete(key string) {
-	j.mu.Lock()
-	delete(j.jobs, key)
-	j.mu.Unlock()
-}
-
-func (j *SharedDockerJobsMap) Add(key string, job DockerJob) {
-	j.mu.Lock()
-	j.jobs[key] = job
-	j.mu.Unlock()
 }
