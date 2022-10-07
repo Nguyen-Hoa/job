6928175a13f8bdfcc77b2b873be34ef4718981d1
kimchi <hnguyen257@ucmerced.edu>
2022-10-07 14:58:32 -0700

    shareJobsMap

--- job_test.go
+++ job_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestSharedAccess(t *testing.T) {
+func TestSharedJobsArrayAccess(t *testing.T) {
 	log.Print("testing SharedJobsArray concurrect access")
 	jobs := SharedJobsArray{}
 
@@ -28,6 +28,44 @@ func TestSharedAccess(t *testing.T) {
 			defer wg.Done()
 			jobs.Pop()
 			log.Println(jobs.Length())
+		}(val)
+	}
+	wg.Wait()
+}
+
+func TestSharedDockerJobsMapAccess(t *testing.T) {
+	log.Print("testing SharedDockerJobsMap concurrect access")
+
+	jobs := SharedDockerJobsMap{}
+	jobs.Init()
+
+	// Try to retrived a key that doesn't exist
+	// `exist` should be false
+	_, exists := jobs.Get("non-existant job")
+	if exists {
+		log.Print("error when accessing non-existant job")
+	}
+
+	// Using goroutines,
+	// Add a few jobs, then delete them
+	count := []string{"one", "two", "three"}
+	var wg sync.WaitGroup
+	for _, val := range count {
+		wg.Add(1)
+		go func(val string) {
+			defer wg.Done()
+			jobs.Update(val, DockerJob{})
+			log.Print(jobs.Length())
+		}(val)
+	}
+	wg.Wait()
+
+	for _, val := range count {
+		wg.Add(1)
+		go func(val string) {
+			defer wg.Done()
+			jobs.Delete(val)
+			log.Print(jobs.Length())
 		}(val)
 	}
 	wg.Wait()
--- sharedJobsMap.go
+++ sharedJobsMap.go
@@ -1,11 +1,16 @@
 package job
 
-func (j *SharedDockerJobsMap) Get(key string) DockerJob {
+func (j *SharedDockerJobsMap) Init() {
+	j.jobs = make(map[string]DockerJob)
+}
+
+func (j *SharedDockerJobsMap) Get(key string) (DockerJob, bool) {
 	var job DockerJob
+	var exists bool
 	j.mu.Lock()
-	job = j.jobs[key]
+	job, exists = j.jobs[key]
 	j.mu.Unlock()
-	return job
+	return job, exists
 }
 
 func (j *SharedDockerJobsMap) Delete(key string) {
@@ -14,8 +19,42 @@ func (j *SharedDockerJobsMap) Delete(key string) {
 	j.mu.Unlock()
 }
 
-func (j *SharedDockerJobsMap) Add(key string, job DockerJob) {
+func (j *SharedDockerJobsMap) Update(key string, job DockerJob) {
 	j.mu.Lock()
 	j.jobs[key] = job
 	j.mu.Unlock()
+}
+
+func (j *SharedDockerJobsMap) Length() int {
+	return len(j.jobs)
+}
+
+func (j *SharedDockerJobsMap) Exists(key string) bool {
+	_, exists := j.jobs[key]
+	return exists
+}
+
+func (j *SharedDockerJobsMap) Refresh(keys []string) {
+	for job := range j.jobs {
+		if !contains(keys, job) {
+			j.Delete(job)
+		}
+	}
+}
+
+func (j *SharedDockerJobsMap) Keys() []string {
+	keys := make([]string, 0, j.Length())
+	for k := range j.jobs {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
